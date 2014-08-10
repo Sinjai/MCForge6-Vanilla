e9a7728c052537a6ebba9f93c2c0ae7160252c6e
UclCommander <luaeditor94@gmail.com>
2014-08-10 16:45:00 -0500

    Database options

--- HtmlData/Resources/js/main.js
+++ HtmlData/Resources/js/main.js
@@ -1,21 +1,25 @@
 $(function() {
 
-    if(public_checked === "true") { $('#is-public').prop('checked', true); }
-    if(loadalllevels_checked === "true") { $('#loadalllevels').prop('checked', true); }
-    if(verifynames_checked === "true") { $('#verifynames').prop('checked', true); }
-    if(verifying_checked === "true") { $('#verifying').prop('checked', true); }
-    if(agreeingtorules_checked === "true") { $('#agreeingtorules').prop('checked', true); }
-    if(irc_enabled_checked === "true") { $('#irc-enabled').prop('checked', true); }
-    if(gc_enabled_checked === "true") { $('#gc-enabled').prop('checked', true); }
-    if(usingconsole_checked === "true") { $('#usingconsole').prop('checked', true); }
-    if(check_for_core_updates_checked === "true") { $('#check-for-core-updates').prop('checked', true); }
-    if(check_misc_updates_checked === "true") { $('#check-misc-updates').prop('checked', true); }
-    if(allow_patch_updates_checked === "true") { $('#allow-patch-updates').prop('checked', true); }
-    if(allow_update_checked === "true") { $('#allow-update').prop('checked', true); }
-    if(silent_update_checked === "true") { $('#silent-update').prop('checked', true); }
-    if(ask_before_core_checked === "true") { $('#ask-before-core').prop('checked', true); }
-    if(ask_before_misc_checked === "true") { $('#ask-before-misc').prop('checked', true); }
-    if(silent_core_update_checked === "true") { $('#silent-core-update').prop('checked', true); }
+    if(public_checked.toLowerCase() === "true") { $('#is-public').prop('checked', true); }
+    if(loadalllevels_checked.toLowerCase() === "true") { $('#loadalllevels').prop('checked', true); }
+    if(verifynames_checked.toLowerCase() === "true") { $('#verifynames').prop('checked', true); }
+    if(verifying_checked.toLowerCase() === "true") { $('#verifying').prop('checked', true); }
+    if(agreeingtorules_checked.toLowerCase() === "true") { $('#agreeingtorules').prop('checked', true); }
+    if(irc_enabled_checked.toLowerCase() === "true") { $('#irc-enabled').prop('checked', true); }
+    if(gc_enabled_checked.toLowerCase() === "true") { $('#gc-enabled').prop('checked', true); }
+    if(usingconsole_checked.toLowerCase() === "true") { $('#usingconsole').prop('checked', true); }
+    if(check_for_core_updates_checked.toLowerCase() === "true") { $('#check-for-core-updates').prop('checked', true); }
+    if(check_misc_updates_checked.toLowerCase() === "true") { $('#check-misc-updates').prop('checked', true); }
+    if(allow_patch_updates_checked.toLowerCase() === "true") { $('#allow-patch-updates').prop('checked', true); }
+    if(allow_update_checked.toLowerCase() === "true") { $('#allow-update').prop('checked', true); }
+    if(silent_update_checked.toLowerCase() === "true") { $('#silent-update').prop('checked', true); }
+    if(ask_before_core_checked.toLowerCase() === "true") { $('#ask-before-core').prop('checked', true); }
+    if(ask_before_misc_checked.toLowerCase() === "true") { $('#ask-before-misc').prop('checked', true); }
+    if(silent_core_update_checked.toLowerCase() === "true") { $('#silent-core-update').prop('checked', true); }
+    if(mysql_pooling_checked.toLowerCase() === "true") { $('#mysql-pooling').prop('checked', true); }
+    if(sqlite_pooling_checked.toLowerCase() === "true") { $('#sqlite-pooling').prop('checked', true); }
+    if(sqlite_inmemory_checked.toLowerCase() === "true") { $('#sqlite-inmemory').prop('checked', true); }
+    if(database_queuing_checked.toLowerCase() === "true") { $('#database-queuing').prop('checked', true); }
 
     $('[data-show-module]').click(function() {
         $("#main").hide();
@@ -55,6 +59,17 @@ $(function() {
 
     $('#save-settings').click(function() {
         $("#form").submit();
+    });
+
+    var $dbselect = $('[data-db-select]');
+
+    $dbselect.find("option[value='" + $dbselect.attr('data-current-value') + "']").prop("selected", true);
+
+    $("#database-" + $dbselect.attr('data-current-value')).show();
+
+    $dbselect.change(function() {
+        $('.db-item').hide();
+        $("#database-" + $(this).val()).show();
     });
 });
 
